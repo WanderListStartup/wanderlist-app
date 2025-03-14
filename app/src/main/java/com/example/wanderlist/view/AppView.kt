562bc4f7e7f7b1578dc74f6eb01e22e99b2d2e96
Joseph Pan <108947845+Enigma-iwnl@users.noreply.github.com>
2025-03-13 20:26:09 -0400

    signup in

--- app/src/main/java/com/example/wanderlist/view/AppView.kt
+++ app/src/main/java/com/example/wanderlist/view/AppView.kt
@@ -49,7 +49,7 @@ fun AppView(){
 //                    Text("LOGIN")
                 }
                 composable<Register> {
-
+                    SignUpView()
                 }
                 composable<Username> {
 
