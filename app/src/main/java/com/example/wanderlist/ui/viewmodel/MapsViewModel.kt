f8845c7839fa599b1a4dc3d1304c31c7c2f43cab
Jimmy Chen <jc219570512@gmail.com>
2025-03-13 21:25:53 -0400

    chanes

--- app/src/main/java/com/example/wanderlist/ui/viewmodel/MapsViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.wanderlist.ui.viewmodel
-
-class MapsViewModel {
-}
--- app/src/main/java/com/example/wanderlist/utils/LocationHelper.kt
+++
@@ -1,9 +0,0 @@
-package com.example.wanderlist.utils
-
-import android.content.Context
-import com.google.android.gms.location.LocationServices
-
-
-class LocationHelper(private val context: Context) {
-    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
-}
